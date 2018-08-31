402a8f47ace2d7caa40802d126f75ee3f2903580
Nader Toukabri <nader@emiketic.com>
2018-08-31 18:43:21 +0100

    fix upload script

---
+++ scripts/config.sh
@@ -0,0 +1,7 @@
+
+PROJECT_DIR=$(pwd)
+
+APP_NAME=HelloWorld
+
+FTP_DOMAIN=ftp.emiketic.com
+FTP_PATH=/starter/vue
---
+++ scripts/upload.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -xeo pipefail
+
+source $(dirname $0)/config.sh
+
+TARGET=$1
+
+if [ -z "$TARGET" ] || [ ! -d "$TARGET" ] || [ -z "$FTP_USERNAME" ] || [ -z "$FTP_PASSWORD" ]; then
+  exit 1
+fi
+
+cd $TARGET
+
+for path in $(find -type f)
+do
+  curl --fail --ftp-create-dirs -u "${FTP_USERNAME}:${FTP_PASSWORD}" "ftp://${FTP_DOMAIN}${FTP_PATH}/${path}" -T $path
+done
