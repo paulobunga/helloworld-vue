0c96b7f486a4703bc81bcd2d4b514fe901afe680
Nader Toukabri <nader@emiketic.com>
2018-09-06 10:36:26 +0100

    fix on production

--- src/common/config.js
+++ src/common/config.js
@@ -14,9 +14,7 @@ if (localStorage && localStorage.CONFIG_OVERRIDE) {
 
 debug('CONFIG_OVERRIDE', CONFIG_OVERRIDE);
 
-// export const API_ENDPOINT_ = CONFIG_OVERRIDE.CONFIG_OVERRIDE
-//   || (process.env.NODE_ENV === 'production' ? 'https://api.helloworld.com' : 'http://localhost:1337');
 export const API_ENDPOINT = CONFIG_OVERRIDE.CONFIG_OVERRIDE
   || (process.env.NODE_ENV === 'production'
-    ? 'https://api.helloworld.com'
+    ? 'http://private-5bf85-starterspecapi.apiary-mock.com'
     : 'http://private-5bf85-starterspecapi.apiary-mock.com');
