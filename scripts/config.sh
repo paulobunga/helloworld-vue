556486f3fd1f9c96272bcca718a1710b87531a20
Nader Toukabri <nader@emiketic.com>
2018-11-14 13:01:18 +0100

    update ci

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,6 +2,3 @@
 PROJECT_DIR=$(pwd)
 
 APP_NAME=HelloWorld
-
-FTP_DOMAIN=ftp.emiketic.com
-FTP_PATH=/starter/vue
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,10 +5,15 @@ source $(dirname $0)/config.sh
 
 TARGET=$1
 
-if [ -z "$TARGET" ] || [ ! -d "$TARGET" ] || [ -z "$FTP_USERNAME" ] || [ -z "$FTP_PASSWORD" ]; then
+if [ -z "$TARGET" ] || [ ! -d "$TARGET" ] ; then
   exit 1
 fi
 
-cd $TARGET
-
-ncftpput -R -v -u "$FTP_USERNAME" -p "$FTP_PASSWORD" "$FTP_DOMAIN" "$FTP_PATH" ./
+if [ -n "$DEPLOY_SSH_TARGET" ] && [ -n "$DEPLOY_SSH_PATH" ]; then
+  rsync -azv -e 'ssh' "$TARGET" "${DEPLOY_SSH_TARGET}:${DEPLOY_SSH_PATH}"
+elif [ -n "$DEPLOY_FTP_DOMAIN" ] && [ "$DEPLOY_FTP_PATH" ] && [ -n "$DEPLOY_FTP_USERNAME" ] && [ -n "$DEPLOY_FTP_PASSWORD" ]; then
+  cd $TARGET
+  ncftpput -mR -v -u "$DEPLOY_FTP_USERNAME" -p "$DEPLOY_FTP_PASSWORD" "$DEPLOY_FTP_DOMAIN" "$DEPLOY_FTP_PATH" ./
+else
+  exit 1
+fi
