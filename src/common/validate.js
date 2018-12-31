835c6c902b62ed5af1fcf56e4ab2900d5719772e
Nader Toukabri <nader@emiketic.com>
2018-12-31 13:06:09 +0100

    update lib

--- src/common/validate.js
+++ src/common/validate.js
@@ -1,6 +1,4 @@
-/**
- * VALIDATION RULES
- */
+export * from 'starter-lib/dist/common/validate';
 
 export const PASSWORD_LENGTH = 8;
 export const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
