254818aa4bf353745e5614b1636e335ed6f7ebb2
Nader Toukabri <nader@emiketic.com>
2019-04-14 19:25:37 +0100

    cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -xeo pipefail
 
-TARGET=$1
+BUILD=$1
+DESTINATION=$2
 
-if [ -z "$TARGET" ] || [ ! -d "$TARGET" ] ; then
+if [ -z "$BUILD" ] || [ ! -d "$BUILD" ] ; then
   exit 1
 fi
 
-if [ -n "$DEPLOY_SSH_TARGET" ] && [ -n "$DEPLOY_SSH_PATH" ]; then
-  rsync -azv -e 'ssh' "$TARGET" "${DEPLOY_SSH_TARGET}:${DEPLOY_SSH_PATH}"
+if [ -n "$DESTINATION" ] && [ -d "$DESTINATION" ]; then
+  rsync -avz "$BUILD" "$DESTINATION"
+elif [ -n "$DEPLOY_SSH_TARGET" ] && [ -n "$DEPLOY_SSH_PATH" ]; then
+  rsync -azv -e 'ssh' "$BUILD" "${DEPLOY_SSH_TARGET}:${DEPLOY_SSH_PATH}"
 elif [ -n "$DEPLOY_FTP_DOMAIN" ] && [ "$DEPLOY_FTP_PATH" ] && [ -n "$DEPLOY_FTP_USERNAME" ] && [ -n "$DEPLOY_FTP_PASSWORD" ]; then
-  cd $TARGET
+  cd $BUILD
   ncftpput -mR -v -u "$DEPLOY_FTP_USERNAME" -p "$DEPLOY_FTP_PASSWORD" "$DEPLOY_FTP_DOMAIN" "$DEPLOY_FTP_PATH" ./
 else
   exit 1
