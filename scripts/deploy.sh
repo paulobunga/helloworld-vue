3d3d9c757b8001a13c063a2bde0c6be017921879
Nader Toukabri <nader@emiketic.com>
2019-04-16 16:51:17 +0100

    update lib ; fix ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,9 +8,9 @@ if [ -z "$BUILD" ] || [ ! -d "$BUILD" ] ; then
 fi
 
 if [ -n "$DEPLOY_PATH" ] && [ -d "$DEPLOY_PATH" ]; then
-  rsync -azv --no-perms --no-owner --no-group "$BUILD" "$DEPLOY_PATH"
+  rsync -rlzv "$BUILD" "$DEPLOY_PATH"
 elif [ -n "$DEPLOY_SSH_TARGET" ] && [ -n "$DEPLOY_SSH_PATH" ]; then
-  rsync -azv --no-perms --no-owner --no-group -e 'ssh' "$BUILD" "${DEPLOY_SSH_TARGET}:${DEPLOY_SSH_PATH}"
+  rsync -rlzv -e 'ssh' "$BUILD" "${DEPLOY_SSH_TARGET}:${DEPLOY_SSH_PATH}"
 elif [ -n "$DEPLOY_FTP_DOMAIN" ] && [ "$DEPLOY_FTP_PATH" ] && [ -n "$DEPLOY_FTP_USERNAME" ] && [ -n "$DEPLOY_FTP_PASSWORD" ]; then
   cd $BUILD
   ncftpput -mR -v -u "$DEPLOY_FTP_USERNAME" -p "$DEPLOY_FTP_PASSWORD" "$DEPLOY_FTP_DOMAIN" "$DEPLOY_FTP_PATH" ./
