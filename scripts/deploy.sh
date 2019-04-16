7c69a64f48588edaa4bc308c7dfe8cdf96e84dd4
Nader Toukabri <nader@emiketic.com>
2019-04-16 11:31:57 +0100

    cleanup ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,13 @@
 set -xeo pipefail
 
 BUILD=$1
-DESTINATION=$2
 
 if [ -z "$BUILD" ] || [ ! -d "$BUILD" ] ; then
   exit 1
 fi
 
-if [ -n "$DESTINATION" ] && [ -d "$DESTINATION" ]; then
-  rsync -avz "$BUILD" "$DESTINATION"
+if [ -n "$DEPLOY_PATH" ] && [ -d "$DEPLOY_PATH" ]; then
+  rsync -avz "$BUILD" "$DEPLOY_PATH"
 elif [ -n "$DEPLOY_SSH_TARGET" ] && [ -n "$DEPLOY_SSH_PATH" ]; then
   rsync -azv -e 'ssh' "$BUILD" "${DEPLOY_SSH_TARGET}:${DEPLOY_SSH_PATH}"
 elif [ -n "$DEPLOY_FTP_DOMAIN" ] && [ "$DEPLOY_FTP_PATH" ] && [ -n "$DEPLOY_FTP_USERNAME" ] && [ -n "$DEPLOY_FTP_PASSWORD" ]; then
